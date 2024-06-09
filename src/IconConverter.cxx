9e1f3974fce63759bea3dc4eac3f4a9438d108e8
mthierman <mthierman@gmail.com>
2024-06-08 23:36:53 -0600

    Add arg parsing

--- src/IconConverter.cxx
+++ src/IconConverter.cxx
@@ -1,23 +1,57 @@
+#include <expected>
+#include <filesystem>
+#include <print>
 #include <string>
 #include <vector>
-#include <print>
-#include <filesystem>
 
 namespace fs = std::filesystem;
 
-auto main(int argc, char* argv[]) -> int
+auto parse_app_name(std::vector<std::string> args) -> std::string
 {
-    std::vector<std::string> args(argv, argc + argv);
+    return fs::path(args.at(0)).filename().replace_extension("").string();
+}
 
-    auto app{fs::path(args.at(0)).filename().replace_extension("")};
+auto parse_arg(std::vector<std::string> args, int position) -> std::expected<fs::path, std::string>
+{
     args.erase(args.begin());
 
-    std::println("{}", app.string());
-
-    for (const auto& arg : args)
+    try
     {
-        std::println("{}", arg);
+        return args.at(position);
+    }
+    catch (const std::out_of_range& e)
+    {
+        switch (position)
+        {
+            case 0:
+                return std::unexpected("No input file specified");
+            case 1:
+                return std::unexpected("No output file specified");
+            default:
+                return std::unexpected("");
+        }
+    }
+}
+
+auto main(int argc, char* argv[]) -> int
+{
+    auto args{std::vector<std::string>(argv, argc + argv)};
+
+    auto appName{parse_app_name(args)};
+    auto inputFile{parse_arg(args, 0)};
+    auto outputFile{parse_arg(args, 1)};
+
+    if (!inputFile.has_value())
+    {
+        std::println("{}", inputFile.error());
+        return EXIT_FAILURE;
     }
 
-    std::println("Test");
+    if (!outputFile.has_value())
+    {
+        std::println("{}", outputFile.error());
+        return EXIT_FAILURE;
+    }
+
+    std::println("Exiting...");
 }
