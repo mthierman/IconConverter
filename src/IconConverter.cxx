db3a3a2f5fff01d99cc82b69f3f9ce0648f95f47
mthierman <mthierman@gmail.com>
2024-06-08 22:50:02 -0600

    Get program name from argv

--- src/IconConverter.cxx
+++ src/IconConverter.cxx
@@ -1,6 +1,23 @@
+#include <string>
+#include <vector>
 #include <print>
+#include <filesystem>
 
-auto main() -> int
+namespace fs = std::filesystem;
+
+auto main(int argc, char* argv[]) -> int
 {
+    std::vector<std::string> args(argv, argc + argv);
+
+    auto app{fs::path(args.at(0)).filename().replace_extension("")};
+    args.erase(args.begin());
+
+    std::println("{}", app.string());
+
+    for (const auto& arg : args)
+    {
+        std::println("{}", arg);
+    }
+
     std::println("Test");
 }
